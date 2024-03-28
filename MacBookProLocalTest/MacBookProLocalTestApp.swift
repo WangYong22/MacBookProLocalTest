670311374cb32c4664d08528c2d79ababc98293c
WangYong22 <wangyong2@gmail.com>
2024-03-28 11:04:11 +0800

    Initial Commit

---
+++ MacBookProLocalTest/MacBookProLocalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacBookProLocalTestApp.swift
+//  MacBookProLocalTest
+//
+//  Created by test on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct MacBookProLocalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
