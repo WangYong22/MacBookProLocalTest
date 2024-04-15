8bfd2130b7796ae529ea6778f06176d4eeba1305
WangYong22 <wangyong2@gmail.com>
2024-04-15 23:30:07 +0800

    2

--- MacBookProLocalTest/ContentView.swift
+++ MacBookProLocalTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, SE 1 world!")
+            Text("Hello, SE 2 world!")
         }
         .padding()
     }
